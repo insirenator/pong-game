d71bd1dfd6149c823ba679a5f776e419a66c6b85
insirenator <arsshakeeb149@protonmail.com>
2022-05-05 11:50:48 +0530

    Added Score board and a center border

--- components.py
+++ components.py
@@ -3,8 +3,10 @@ import os
 
 PADDLE_IMG = pygame.image.load(os.path.join("assets","paddle.png"))
 BALL_IMG = pygame.image.load(os.path.join("assets","ball.png"))
+GAME_OVER_IMG = pygame.transform.scale(pygame.image.load(os.path.join("assets","game_over.png")),(400,60))
 
 class Paddle:
+	'''Represents the game paddle'''
 	def __init__(self, width, height, coords):
 		self.img = pygame.transform.scale(PADDLE_IMG,(width, height))
 		self.width = width
@@ -12,17 +14,21 @@ class Paddle:
 		self.x, self.y = coords
 
 	def draw(self, SCREEN):
+		'''Draw paddle on the screen'''
 		SCREEN.blit(self.img,(self.x, self.y))
 
 	def move_up(self, VEL):
+		'''Move the paddle up'''
 		if self.y - VEL > 0 :
 			self.y -= VEL
 
 	def move_down(self, VEL, HEIGHT):
+		'''Move the paddle down'''
 		if self.y + VEL + self.height < HEIGHT :
 			self.y += VEL
 
 class Ball:
+	'''Represents the Ball'''
 	def __init__(self, diameter, coords, velocity):
 		self.img = pygame.transform.scale(BALL_IMG,(diameter,diameter))
 		self.VEL = velocity
@@ -31,4 +37,5 @@ class Ball:
 		self.x, self.y = coords
 
 	def draw(self, SCREEN):
+		'''Draw the ball on the screen'''
 		SCREEN.blit(self.img,(self.x, self.y))
--- pong.py
+++ pong.py
@@ -1,10 +1,19 @@
 import pygame
+from random import choice
+
 from components import Paddle
 from components import Ball
+from components import GAME_OVER_IMG
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+
 
 class Pong:
+	'''Main Game Class'''
 	def __init__(self):
-		self.WIDTH,self.HEIGHT = 600,500
+		pygame.init()
+		self.WIDTH,self.HEIGHT = 800,600
 		self.VEL = 5
 		self.SCREEN = pygame.display.set_mode((self.WIDTH,self.HEIGHT))
 		
@@ -12,13 +21,39 @@ class Pong:
 
 		self.paddle_L = Paddle(10,150,(0,0))
 		self.paddle_R = Paddle(10,150,(self.WIDTH-11,0))
-		self.ball = Ball(15,(300,250), 10)
+		self.ball = Ball(15,(self.WIDTH/2, self.HEIGHT/2), 10)
+
+		self.center_border = pygame.Rect(self.WIDTH/2 - 1, 0, 2, self.HEIGHT)
+
+		#Paddle Hits
+		self.left_hits = 0
+		self.right_hits = 0
+
+		#Text Objects
+		self.FONT = pygame.font.Font('freesansbold.ttf',28)
+		self.right_score = self.FONT.render(str(self.right_hits), True, WHITE, BLACK)
+		self.left_score = self.FONT.render(str(self.left_hits), True, WHITE, BLACK) 
+
+		self.l_text_rect = self.left_score.get_rect()
+		self.l_text_rect.center = ((self.WIDTH/4)*3, 20)
+		self.r_text_rect = self.right_score.get_rect()
+		self.r_text_rect.center = (self.WIDTH/4, 20)
 
 		#Translation Factors for ball motion
-		self.tf_x = 1
-		self.tf_y = 1
+		self.tf_x = choice([1,-1])
+		self.tf_y = choice([1,-1])
+
+	def display_score(self):
+		self.right_score = self.FONT.render(str(self.right_hits), True, WHITE, BLACK)
+		self.left_score = self.FONT.render(str(self.left_hits), True, WHITE, BLACK)
+		self.SCREEN.blit(self.left_score, self.l_text_rect)
+		self.SCREEN.blit(self.right_score, self.r_text_rect)
+
+	def draw_center_border(self):
+		pygame.draw.rect(self.SCREEN, (255,255,255), self.center_border)
 
 	def handle_movement(self, key_pressed):
+		'''Respond to the keys pressed'''
 		#Left Paddle
 		if key_pressed[pygame.K_w]:
 			self.paddle_L.move_up(self.VEL)
@@ -32,6 +67,7 @@ class Pong:
 			self.paddle_R.move_down(self.VEL, self.HEIGHT)
 
 	def ball_outbound(self, ball):
+		'''Check if ball is out of bound'''
 		if ball.x < 0 or ball.x > self.WIDTH :
 			return True
 		elif ball.y < 0 or ball.y > self.HEIGHT :
@@ -39,9 +75,8 @@ class Pong:
 		else:
 			return False
 
-
-
 	def translate_ball(self,ball):
+		'''Control the motion of the ball'''
 		if ball.y + ball.height + self.tf_y > self.HEIGHT:
 			self.tf_y = -(self.tf_y)
 		if ball.y + self.tf_y < 0 :
@@ -49,9 +84,10 @@ class Pong:
 
 		if ball.x + ball.width + self.tf_x > self.WIDTH and self.paddle_R.y < ball.y < self.paddle_R.y + self.paddle_R.height:
 			self.tf_x = -(self.tf_x)
+			self.left_hits += 1 # Update Left Hits
 		if ball.x + self.tf_x < 0 and self.paddle_L.y < ball.y < self.paddle_L.y + self.paddle_L.height:
 			self.tf_x = -(self.tf_x)
-
+			self.right_hits += 1 # Update Right Hits
 
 
 		ball.x += self.tf_x
@@ -59,6 +95,7 @@ class Pong:
 
 
 	def run_game(self):
+		'''Game Driver'''
 		clock = pygame.time.Clock()
 		FPS = 120
 		run = True
@@ -67,18 +104,27 @@ class Pong:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					run = False
+					pygame.quit()
 
 			key_pressed = pygame.key.get_pressed()
 			self.handle_movement(key_pressed)
 			self.SCREEN.fill((0,0,0))
+			self.draw_center_border()
 			self.paddle_L.draw(self.SCREEN)
 			self.paddle_R.draw(self.SCREEN)
 			self.translate_ball(self.ball)
 			self.ball.draw(self.SCREEN)
+			self.display_score()
 			pygame.display.update()
 
 			if self.ball_outbound(self.ball):
 				run = False
 
+		self.SCREEN.blit(GAME_OVER_IMG,(self.WIDTH/2-200,self.HEIGHT/2-60))
+		pygame.display.update()
 
-		pygame.quit()		
+		pygame.event.clear()
+		while True:
+			event = pygame.event.wait()
+			if event.type == pygame.QUIT:
+				pygame.quit()		
